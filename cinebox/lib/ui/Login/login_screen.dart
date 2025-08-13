9b40def71bc7cec958bb1c50b3cc2ea70ec27602
Guilherme Moreira dos Santos <guilherme.vtm2@gmail.com>
2025-08-13 16:31:04 -0300

    ADD: Construindo Tela de Login

--- cinebox/lib/ui/Login/login_screen.dart
+++ cinebox/lib/ui/Login/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cinebox/ui/Login/widgets/sign_in_google_button.dart';
+import 'package:cinebox/ui/core/themes/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,7 +14,34 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Login'),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            color: Colors.black.withAlpha(170),
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            padding: EdgeInsets.only(top: 108),
+            child: Column(
+              spacing: 48,
+              children: [
+                Image.asset(R.ASSETS_IMAGES_LOGO_PNG) ,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: SignInGoogleButton(),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
