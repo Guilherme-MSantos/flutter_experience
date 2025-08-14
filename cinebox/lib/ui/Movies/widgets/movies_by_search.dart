48e108ce2fdf462fcfdefab4db997a786b79b2bb
Guilherme Moreira dos Santos <guilherme.vtm2@gmail.com>
2025-08-14 15:34:52 -0300

    [Movie Screen] - Filmes por busca de texto

--- cinebox/lib/ui/Movies/widgets/movies_by_search.dart
+++ cinebox/lib/ui/Movies/widgets/movies_by_search.dart
@@ -2,14 +2,14 @@ import 'package:cinebox/ui/Movies/widgets/movies_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MoviesByGenre extends ConsumerStatefulWidget {
-  const MoviesByGenre({super.key});
+class MoviesBySearch extends ConsumerStatefulWidget {
+  const MoviesBySearch({super.key});
 
   @override
-  ConsumerState<MoviesByGenre> createState() => _MoviesByGenreState();
+  ConsumerState<MoviesBySearch> createState() => _MoviesBySearchState();
 }
 
-class _MoviesByGenreState extends ConsumerState<MoviesByGenre> {
+class _MoviesBySearchState extends ConsumerState<MoviesBySearch> {
   @override
   Widget build(BuildContext context) {
     return Container(
