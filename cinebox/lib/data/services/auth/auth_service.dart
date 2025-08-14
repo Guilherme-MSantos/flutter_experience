e92b65ff603205a24b2d5d44379434812d0f4d9f
Guilherme Moreira dos Santos <guilherme.vtm2@gmail.com>
2025-08-14 10:28:18 -0300

    corrige erros

--- cinebox/lib/data/services/auth/auth_service.dart
+++ cinebox/lib/data/services/auth/auth_service.dart
@@ -1,12 +1,12 @@
-import 'package:retrofit/error_logger.dart';
-import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart' ;
 import 'package:dio/dio.dart';
+
 part 'auth_service.g.dart';
 
 @RestApi()
 abstract class AuthService {
 
-  factory AuthService(Dio dio, {String? baseUrl}) = _AuthService ;
+   factory AuthService(Dio dio, {String? baseUrl}) = _AuthService ;
 
 @POST('/auth')
 Future<void> auth() ;
