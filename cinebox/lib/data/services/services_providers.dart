1c9b08107e4b54caf5f4910a6a5175d0ad808d9d
Guilherme Moreira dos Santos <guilherme.vtm2@gmail.com>
2025-08-15 10:58:35 -0300

    Movie By Category [ Use Case ]

--- cinebox/lib/data/services/services_providers.dart
+++ cinebox/lib/data/services/services_providers.dart
@@ -1,8 +1,10 @@
 import 'package:cinebox/data/core/rest_client/backend_rest_client.dart';
+import 'package:cinebox/data/core/rest_client/tmdb_rest_client.dart';
 import 'package:cinebox/data/services/auth/auth_service.dart';
 import 'package:cinebox/data/services/google_sign_in/google_signin_service_impl.dart';
 import 'package:cinebox/data/services/local_storage/local_storage_service.dart';
 import 'package:cinebox/data/services/local_storage/local_storage_service_impl.dart';
+import 'package:cinebox/data/services/tmdb/tmdb_service.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -22,4 +24,10 @@ LocalStorageService localStorageService(Ref ref){
 @riverpod
 AuthService authService(Ref ref){
   return AuthService(ref.read(backendRestClientProvider)) ;
+}
+
+
+@riverpod
+TmdbService tmdbService(Ref ref){
+  return TmdbService(ref.read(tmdbRestClientProvider)) ;
 }
