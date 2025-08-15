cb25220344c384ec5a59226745d1276af58d5b69
Guilherme Moreira dos Santos <guilherme.vtm2@gmail.com>
2025-08-15 15:31:03 -0300

    Buscar Generos e Apresentar no GendersBox

--- cinebox/lib/data/repositories/tmdb/tmdb_repository_impl.dart
+++ cinebox/lib/data/repositories/tmdb/tmdb_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:cinebox/core/result/result.dart';
 import 'package:cinebox/data/exception/data_exception.dart';
 import 'package:cinebox/data/mappers/movie_mappers.dart';
 import 'package:cinebox/data/services/tmdb/tmdb_service.dart';
+import 'package:cinebox/domain/models/genre.dart';
 
 import 'package:cinebox/domain/models/movie.dart';
 
@@ -87,5 +88,18 @@ class TmdbRepositoryImpl implements TmdbRepository {
       log('ERRO ao buscar getUpcomingMovies',error: e , stackTrace: s) ;
       return Failure(DataException(message: 'Erro ao buscar os filmes  que virão'));
     }
+  }
+
+  @override
+  Future<Result<List<Genre>>> getGenres() async {
+  try {
+    final data = await _tmdbService.getMoviesGenres() ;
+    final genres = data.genres.map((g)=> Genre(id: g.id, name: g.name)).toList();
+    
+    return Success(genres) ; 
+  } on DioException catch (e,s) {
+    log('ERRO ao buscar gênero.',error: e,stackTrace: s) ;
+    return  Failure(DataException(message: 'Erro ao buscar gêneros de filmes')) ;
+  }
   }
 }
